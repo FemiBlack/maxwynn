4c774b23df4f3b18ca8d115874880dc392cb89f7
FemiBlack <tadefemi2@hotmail.com>
2024-01-17 16:55:38 +0100

    update artists section style

--- app/ui/dashboard/artist.tsx
+++ app/ui/dashboard/artist.tsx
@@ -3,16 +3,19 @@ import Image from "next/image";
 
 export default function Artist({ artist }: { artist: Artist }) {
   return (
-    <div>
-      <p>{artist.name}</p>
-      <p>{artist.popularity}</p>
-      <p>{artist.followers.total}</p>
-      <Image
-        src={artist.images[0].url}
-        width={200}
-        height={200}
-        alt={`${artist.name}'s profile pic`}
-      />
-    </div>
+    <article className="p-3 bg-zinc-100 rounded-lg grid place-items-center">
+      <div className="min-w-0 w-full">
+        <Image
+          src={artist.images[0].url}
+          width={200}
+          height={200}
+          className="rounded-full aspect-square object-cover"
+          alt={`${artist.name}'s profile pic`}
+        />
+        <p className="font-semibold text-lg mt-3 truncate text-center">
+          {artist.name}
+        </p>
+      </div>
+    </article>
   );
 }
--- app/ui/dashboard/top-artists.tsx
+++ app/ui/dashboard/top-artists.tsx
@@ -7,9 +7,11 @@ export default async function TopArtists() {
   return (
     <div>
       <h2 className="font-bold text-2xl">Top Artists</h2>
-      {artists.items?.map((artist) => (
-        <Artist artist={artist} key={artist.id} />
-      ))}
+      <div className="grid grid-cols-6 gap-4 mt-3">
+        {artists.items?.map((artist) => (
+          <Artist artist={artist} key={artist.id} />
+        ))}
+      </div>
     </div>
   );
 }
