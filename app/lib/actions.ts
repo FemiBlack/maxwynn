443bc875204c78792191c81c3d4f4041971db0b3
FemiBlack <tadefemi2@hotmail.com>
2024-01-17 17:56:51 +0100

    style login page

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -1,17 +1,15 @@
 "use server";
 
-import crypto from "crypto";
-import { redirect } from "next/navigation";
 import sdk from "./sdk";
+import { signIn } from "@/auth";
+import { AuthError } from "next-auth";
 
 const client_id = process.env.SPOTIFY_CLIENT_ID;
 const client_secret = process.env.SPOTIFY_CLIENT_SECRET;
 const refresh_token = process.env.SPOTIFY_REFRESH_TOKEN;
-const redirect_uri = "http://localhost:3000";
 
 const basic = btoa(`${client_id}:${client_secret}`);
 const NOW_PLAYING_ENDPOINT = `https://api.spotify.com/v1/me/player/currently-playing`;
-const TOP_TRACKS_ENDPOINT = `https://api.spotify.com/v1/me/top/tracks`;
 const TOKEN_ENDPOINT = `https://accounts.spotify.com/api/token`;
 
 const getAccessToken = async () => {
@@ -47,3 +45,19 @@ export const getTopTracks = async () => {
 export const getTopArtists = async () => {
   return sdk.currentUser.topItems("artists");
 };
+
+export async function authenticate() {
+  try {
+    await signIn("spotify");
+  } catch (error) {
+    if (error instanceof AuthError) {
+      switch (error.type) {
+        case "CredentialsSignin":
+          return "Invalid credentials.";
+        default:
+          return "Something went wrong.";
+      }
+    }
+    throw error;
+  }
+}
--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,14 +1,91 @@
-import { signIn } from "@/auth";
+"use client";
+
+import {
+  PauseIcon,
+  PlayIcon,
+  SpeakerWaveIcon,
+  SpeakerXMarkIcon,
+} from "@heroicons/react/24/outline";
+import Image from "next/image";
+import SpotifyLogo from "../ui/spotify-logo";
+import { useRef, useState } from "react";
+import { authenticate } from "../lib/actions";
 
 export default function Page() {
+  const audioRef = useRef<HTMLVideoElement>(null);
+  const [isMuted, setIsMuted] = useState(true);
+  const [isPaused, setIsPaused] = useState(true);
+
+  const toggleMute = () => {
+    if (audioRef.current) {
+      audioRef.current.muted = !audioRef.current.muted;
+      setIsMuted(audioRef.current.muted);
+    }
+  };
+
+  const togglePlay = () => {
+    if (audioRef.current) {
+      if (audioRef.current.paused) {
+        audioRef.current.play();
+      } else {
+        audioRef.current.pause();
+      }
+      setIsPaused(!audioRef.current.paused);
+    }
+  };
   return (
-    <form
-      action={async () => {
-        "use server";
-        await signIn("spotify");
-      }}
-    >
-      <button type="submit">Sign In with spotify</button>
-    </form>
+    <main className="grid place-items-center place-content-center h-screen">
+      <video
+        className="fixed object-cover w-full h-full -z-10"
+        autoPlay
+        muted
+        loop
+        ref={audioRef}
+      >
+        <source src="/drake-og-vid.mp4" type="video/mp4" />
+      </video>
+      <form action={authenticate} className="flex flex-col items-center">
+        <div className="flex gap-3">
+          <button
+            className="rounded-full bg-zinc-900/50 h-20 w-20 flex justify-center items-center"
+            type="button"
+            onClick={toggleMute}
+          >
+            {isMuted ? (
+              <SpeakerXMarkIcon className="h-16 w-16 text-gray-100" />
+            ) : (
+              <SpeakerWaveIcon className="h-16 w-16 text-gray-100" />
+            )}
+          </button>
+          <button
+            className="rounded-full bg-zinc-900/50 h-20 w-20 flex justify-center items-center"
+            type="button"
+            onClick={togglePlay}
+          >
+            {isPaused ? (
+              <PauseIcon className="h-16 w-16 text-gray-100" />
+            ) : (
+              <PlayIcon className="h-16 w-16 text-gray-100" />
+            )}
+          </button>
+        </div>
+        <div className="flex items-center gap-3 mt-4">
+          <Image
+            src="/amw-logo.png"
+            alt="AMW Logo"
+            width={100}
+            height={100}
+            priority
+          />
+          <button
+            type="submit"
+            className="bg-green-400 rounded-full p-4 text-zinc-100 font-bold text-2xl flex gap-2 items-center"
+          >
+            <SpotifyLogo />
+            Login with Spotify
+          </button>
+        </div>
+      </form>
+    </main>
   );
 }
