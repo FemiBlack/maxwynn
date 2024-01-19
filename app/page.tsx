76968e39b9d5c9ad04209f2e412026688a86f571
FemiBlack <tadefemi2@hotmail.com>
2024-01-19 12:53:08 +0100

    add some content to homepage

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,5 @@
 import Image from "next/image";
+import Link from "next/link";
 
 export default function Home() {
   return (
@@ -11,6 +12,11 @@ export default function Home() {
         height={24}
         priority
       />
+
+      <p>I have no clue what i started off here, but please..</p>
+      <Link href="/login" className="py-3 px-8 bg-green-400 rounded-full">
+        Login
+      </Link>
     </main>
   );
 }
