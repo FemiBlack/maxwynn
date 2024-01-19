e0c27fc97c97b0a19f2f2da72750d573df723daa
FemiBlack <tadefemi2@hotmail.com>
2024-01-19 12:55:46 +0100

    fix middleware matcher to allow media files

--- middleware.ts
+++ middleware.ts
@@ -4,5 +4,5 @@ export const { auth: middleware } = NextAuth(authConfig);
 
 // Read more: https://nextjs.org/docs/app/building-your-application/routing/middleware#matcher
 export const config = {
-  matcher: ["/((?!api|_next/static|_next/image|favicon.ico).*)"],
+  matcher: ["/((?!api|_next/static|_next/image|.*\\.png$|.*\\.mp3$).*)"],
 };
